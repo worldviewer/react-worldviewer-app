0feecdc8cd880b3aa3e8d9c404135e2b661b6f91
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-06-25 15:09:42 -0700

    Removed unused redux

--- src/redux.js
+++ src/redux.js
@@ -10,30 +10,9 @@ const types = {
 	SET_USER_TOKEN_LOADING: 'SET_USER_TOKEN_LOADING',
 	UNSET_USER_TOKEN_LOADING: 'UNSET_USER_TOKEN_LOADING',
 
-	SHOW_BUBBLE: 'SHOW_BUBBLE',
-	SPIN_BUBBLE_NUMBER: 'SPIN_BUBBLE_NUMBER',
-	DISABLE_SPIN_BUBBLE_NUMBERS: 'DISABLE_SPIN_BUBBLE_NUMBERS',
-	SET_SPIN_BUBBLE_NUMBER_TIMEOUT: 'SET_SPIN_BUBBLE_NUMBER_TIMEOUT',
-	DEACTIVATE_BUBBLE: 'DEACTIVATE_BUBBLE',
-	CLICK_BUBBLE: 'CLICK_BUBBLE',
-
 	CLICK_ICON: 'CLICK_ICON',
 	OPEN_MENU: 'OPEN_MENU',
 	CLOSE_MENU: 'CLOSE_MENU',
-
-	TOGGLE_OVERLAY_STATE: 'TOGGLE_OVERLAY_STATE',
-	SHADE_ELEMENTS: 'SHADE_ELEMENTS',
-	UNSHADE_ELEMENTS: 'UNSHADE_ELEMENTS',
-	RESET_ELEMENT_ZINDEXES: 'RESET_ELEMENT_ZINDEXES',
-
-	NEXT_SLIDE: 'NEXT_SLIDE',
-	PREV_SLIDE: 'PREV_SLIDE',
-
-	SET_CURRENT_QUOTE: 'SET_CURRENT_QUOTE',
-	SET_CURRENT_QUOTE_ELEMENT: 'SET_CURRENT_QUOTE_ELEMENT',
-	SET_CURRENT_QUOTE_TIMER: 'SET_CURRENT_QUOTE_TIMER',
-	TOGGLE_QUOTE: 'TOGGLE_QUOTE',
-	CLEAR_QUOTE_TIMERS: 'CLEAR_QUOTE_TIMERS',
 
 	FETCH_CARD_REQUEST: 'FETCH_CARD_REQUEST',
 	FETCH_CARD_ERROR: 'FETCH_CARD_ERROR',
@@ -58,11 +37,6 @@ const initialState = {
 		api: 'https://czlxg9sj34.execute-api.us-east-1.amazonaws.com/dev/cards/',
 		background: 'https://controversy-cards-assets.s3.amazonaws.com/',
 		overlay: 'https://controversy-cards-assets.s3.amazonaws.com/'
-	},
-
-	controls: {
-		next: true,
-		prev: false
 	},
 
 	menu: {
@@ -119,37 +93,6 @@ const initialState = {
 		icon: 'https://controversy-cards-assets.s3.amazonaws.com/58b8f1f7b2ef4ddae2fb8b17/icon/'
 	},
 
-	overlays: {
-		active: true,
-		loaded: false
-	},
-
-	slides: {
-		current: 0,
-		previous: null, // This is necessary for TweenMax transitions
-		firsts: {},
-		active: false,
-		num: 0
-	},
-
-	slideshow: [],
-
-	bubbles: { 
-		display: Array.from({length:8}, el => false) // Used on component mount
-	},
-
-	bubbleNumbers: {
-		active: Array.from({length:8}, el => false),
-		timeouts: Array.from({length:8}, el => 0)
-	},
-
-	quotes: {
-		element: null,
-		current: 0,
-		active: false,
-		id: null
-	},
-
 	feeds: {
 		worldview: [],
 		model: [],
@@ -183,34 +126,6 @@ export const setUserTokenLoading = () => {
 export const unsetUserTokenLoading = () => {
 	return {
 		type: types.UNSET_USER_TOKEN_LOADING
-	};
-};
-
-export const showBubble = (num) => {
-	return {
-		type: types.SHOW_BUBBLE,
-		num
-	};
-};
-
-export const spinBubbleNumber = (num) => {
-	return {
-		type: types.SPIN_BUBBLE_NUMBER,
-		num
-	};
-};
-
-export const disableSpinBubbleNumbers = () => {
-	return {
-		type: types.DISABLE_SPIN_BUBBLE_NUMBERS
-	};
-};
-
-export const setSpinBubbleNumberTimeout = (num, timeout) => {
-	return {
-		type: types.SET_SPIN_BUBBLE_NUMBER_TIMEOUT,
-		num,
-		timeout
 	};
 };
 
@@ -268,13 +183,6 @@ export function fetchCard(id, url) {
 	};
 };
 
-export const clickBubble = (num) => {
-	return {
-		type: types.CLICK_BUBBLE,
-		num
-	};
-};
-
 export const openMenu = () => {
 	return {
 		type: types.OPEN_MENU
@@ -284,85 +192,6 @@ export const openMenu = () => {
 export const closeMenu = () => {
 	return {
 		type: types.CLOSE_MENU
-	};
-};
-
-export const toggleOverlayState = (zoom) => {
-	return {
-		type: types.TOGGLE_OVERLAY_STATE,
-		active: zoom <= 1.1
-	};
-};
-
-export const shadeElements = (elements, shade) => {
-	return {
-		type: types.SHADE_ELEMENTS,
-		elements,
-		shade
-	};
-};
-
-export const unshadeElements = () => {
-	return {
-		type: types.UNSHADE_ELEMENTS
-	};
-};
-
-export const resetElementZindexes = () => {
-	return {
-		type: types.RESET_ELEMENT_ZINDEXES
-	};
-};
-
-export const nextSlide = () => {
-	return {
-		type: types.NEXT_SLIDE
-	};
-};
-
-export const prevSlide = () => {
-	return {
-		type: types.PREV_SLIDE
-	};
-};
-
-export const setCurrentQuote = (current) => {
-	return {
-		type: types.SET_CURRENT_QUOTE,
-		current
-	};
-};
-
-export const setCurrentQuoteElement = (element) => {
-	return {
-		type: types.SET_CURRENT_QUOTE_ELEMENT,
-		element
-	};
-};
-
-export const setCurrentQuoteTimer = (id) => {
-	return {
-		type: types.SET_CURRENT_QUOTE_TIMER,
-		id
-	};
-};
-
-export const toggleQuote = (state) => {
-	return {
-		type: types.TOGGLE_QUOTE,
-		state
-	};
-};
-
-export const clearQuoteTimers = () => {
-	return {
-		type: types.CLEAR_QUOTE_TIMERS
-	};
-};
-
-export const deactivateBubble = () => {
-	return {
-		type: types.DEACTIVATE_BUBBLE
 	};
 };
 
@@ -440,57 +269,6 @@ export default (state = initialState, action) => {
 					tokenLoading: false
 				}
 			};
-			
-		case types.SHOW_BUBBLE:
-			let newDisplayState = state.bubbles.display.slice();
-			newDisplayState[action.num] = true;
-
-			return {
-				...state,
-				bubbles: {
-					...state.bubbles,
-					display: newDisplayState
-				}
-			};
-
-		case types.SPIN_BUBBLE_NUMBER:
-			let newSpinState = Array.from({length:8}, el => false);
-			newSpinState[action.num] = true;
-
-			return {
-				...state,
-				bubbleNumbers: {
-					...state.bubbleNumbers,
-					active: newSpinState
-				}
-			};
-
-		case types.DISABLE_SPIN_BUBBLE_NUMBERS:
-			const
-				noSpin = Array.from({length:8}, el => false),
-				noSpinTimeouts = Array.from({length:8}, el => 0);
-
-			return {
-				...state,
-				bubbleNumbers: {
-					...state.bubbleNumbers,
-					active: noSpin,
-					timeouts: noSpinTimeouts
-				}
-			};
-
-		case types.SET_SPIN_BUBBLE_NUMBER_TIMEOUT:
-			let newSpinTimeoutsState = state.bubbleNumbers.timeouts.slice();
-			newSpinTimeoutsState[action.num] = action.timeout;
-
-			return {
-				...state,
-				bubbleNumbers: {
-					...state.bubbleNumbers,
-					active: state.bubbleNumbers.active,
-					timeouts: newSpinTimeoutsState
-				}
-			};
 
 		case types.FETCH_CARD_ERROR:
 			console.log(action.error);
@@ -528,61 +306,6 @@ export default (state = initialState, action) => {
 				}
 			};
 
-		case types.CLICK_BUBBLE:
-			const current = state.slides.firsts[action.num];
-
-			isEnd = current === state.slideshow.length-1;
-			isBeginning = current === 0;
-
-			if (isEnd) {
-				controls = {
-					next: false,
-					prev: true					
-				};
-			} else if (isBeginning) {
-				controls = {
-					next: true,
-					prev: false
-				};
-			} else {
-				controls = {
-					next: true,
-					prev: true
-				};
-			}
-
-			return {
-				...state,
-				slides: {
-					...state.slides,
-					active: !state.slides.active,
-					current: state.slides.firsts[action.num],
-					previous: {
-						...state.slideshow[state.slides.current].bubble || null,
-						num: state.slides.current
-					}
-				},
-				controls: controls
-			};
-
-		case types.DEACTIVATE_BUBBLE:
-			// If the user has deactivated the slide, then advance current to next bubble
-			let jump = state.slides.current
-			const firstSlides = Object.values(state.slides.firsts);
-
-			while (!firstSlides.includes(jump) && jump < state.slides.num) {
-				++jump
-			}
-
-			return {
-				...state,
-				slides: {
-					...state.slides,
-					current: jump-1,
-					active: false
-				}
-			};
-
 		case types.OPEN_MENU:
 			return {
 				...state,
@@ -598,200 +321,6 @@ export default (state = initialState, action) => {
 				menu: {
 					...state.menu,
 					open: false
-				}
-			};
-
-		case types.TOGGLE_OVERLAY_STATE:
-			return {
-				...state,
-				overlays: {
-					...state.overlays,
-					active: action.active
-				}
-			};
-
-		// Note: shade display toggles to none when shade === 0
-		case types.SHADE_ELEMENTS:
-			zindexes = { ...state.card.zindexes };
-
-			zindexes = { ...state.card.zindexes };
-
-			if (action.elements === 'all') {
-				for (let zindex in state.card.zindexes) {
-					if (state.card.zindexes.hasOwnProperty(zindex)) {
-						zindexes[zindex] = -10;
-					}
-				}
-
-			// Important: shades all but the passed elements
-			} else {
-				for (let zindex in state.card.zindexes) {
-					if (state.card.zindexes.hasOwnProperty(zindex)) {
-						if (action.elements.includes(zindex)) {
-							zindexes[zindex] = 10;
-						} else {
-							zindexes[zindex] = -10;
-						}
-					}
-				}
-			}
-
-			return {
-				...state,
-				card: {
-					...state.card,
-					zindexes,
-					shade: {
-						darkness: action.shade,
-						zindex: 5
-					}
-				}
-			};
-
-		case types.UNSHADE_ELEMENTS:
-			return {
-				...state,
-				card: {
-					...state.card,
-					shade: {
-						...state.card.shade,
-						darkness: 0
-					}
-				}
-			};
-
-		case types.RESET_ELEMENT_ZINDEXES:
-			zindexes = { ...state.card.zindexes };
-
-			for (let zindex in state.card.zindexes) {
-				if (state.card.zindexes.hasOwnProperty(zindex)) {
-					zindexes[zindex] = 10;
-				}
-			}
-
-			return {
-				...state,
-				card: {
-					...state.card,
-					zindexes,
-					shade: {
-						...state.card.shade,
-						zindex: 0
-					}
-				}
-			};		
-
-		case types.NEXT_SLIDE:
-			isEnd = state.slides.current === state.slideshow.length-1;
-
-			const
-				next = isEnd ?
-				state.slides.current :
-				state.slides.current+1;
-
-			controls = isEnd ?
-				{
-					next: false,
-					prev: true
-				} :
-				{
-					next: true,
-					prev: true
-				};
-
-			return {
-				...state,
-				slides: {
-					...state.slides,
-					active: true,
-					current: next,
-					previous: {
-						...state.slideshow[state.slides.current].bubble || null,
-						num: state.slides.current
-					}
-				},
-				controls: controls,
-				quotes: {
-					...state.quotes,
-					current: 0
-				}
-			};
-
-		case types.PREV_SLIDE:
-			isBeginning = state.slides.current === 0;
-
-			const
-				previous = isBeginning ? 0 :
-				state.slides.current-1;
-
-			controls = isBeginning ?
-				{
-					next: true,
-					prev: false
-				} :
-				{
-					next: true,
-					prev: true
-				};
-
-			return {
-				...state,
-				slides: {
-					...state.slides,
-					active: true,
-					current: previous,
-					previous: {
-						...state.slideshow[state.slides.current].bubble || null,
-						num: state.slides.current
-					}
-				},
-				controls: controls
-			};
-
-		case types.SET_CURRENT_QUOTE:
-			return {
-				...state,
-				quotes: {
-					...state.quotes,
-					current: action.current
-				}
-			};
-
-		case types.SET_CURRENT_QUOTE_ELEMENT:
-			return {
-				...state,
-				quotes: {
-					...state.quotes,
-					element: action.element
-				}
-			};		
-
-		case types.SET_CURRENT_QUOTE_TIMER:
-			return {
-				...state,
-				quotes: {
-					...state.quotes,
-					id: action.id
-				}
-			};
-
-		case types.TOGGLE_QUOTE:
-			return {
-				...state,
-				quotes: {
-					...state.quotes,
-					active: action.state
-				}
-			};
-
-		case types.CLEAR_QUOTE_TIMERS:
-			return {
-				...state,
-				quotes: {
-					...state.quotes,
-					current: 0,
-					id: null,
-					active: false
 				}
 			};
 
