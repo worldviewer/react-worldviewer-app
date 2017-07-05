10f2e139275ed4e0b937876fc59a53528c6bcaa5
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-07-05 13:14:21 -0500

    Switched from material UI to bootstrap, fixed errors in card view

--- src/components/FeedCard/FeedCardComponent.jsx
+++ src/components/FeedCard/FeedCardComponent.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import './FeedCard.css';
 import share from './share.svg';
-import Avatar from 'material-ui/Avatar';
-import Chip from 'material-ui/Chip';
 import chris from './chris.jpg';
 import 'font-awesome-sass-loader';
 import { withRouter } from 'react-router-dom';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Badge, Image } from 'react-bootstrap';
 
 // Permits HTML markup encoding in feed text
 import { Parser as HtmlToReactParser } from 'html-to-react';
@@ -36,94 +34,92 @@ class FeedCardComponent extends Component {
 		const
 			h = new HtmlToReactParser(),
 
-			chipStyles = {
-				chip: {
-					margin: 4,
-					fontFamily: ['Lato', 'Helvetica']
-				},
-				wrapper: {
-					display: 'flex',
-					flexWrap: 'wrap'
-				},
-			},
-
 			imageStyles = this.state.isTextExpanded ?
 				{ height: 0 } :
 				{},
 
-			outerShareStyles = this.state.isTextExpanded ?
+			chipStyles = {
+				display: 'flex',
+				flexWrap: 'wrap'
+			},
+
+			shareStyles = this.state.isTextExpanded ?
+				{ display: 'none' } :
 				{
-					width: 0,
-				 	height: 0,
-				 	opacity: 0,
-				 	margin: '18px' } :
-				{
-					margin: 0,
-					opacity: 0.6
+					backgroundColor: '#1D1D1D',
+					cursor: 'pointer',
+					left: '30px',
+					height: '36px',
+					padding: '3px',
+					position: 'relative',
+					top: '30px',
+					width: '36px',
+					zIndex: 10
 				},
 
-			innerShareStyles = this.state.isTextExpanded ?
-				{
-					width: 0,
-				 	height: 0,
-				 	opacity: 0 } :
-				{
-					margin: 0,
-					opacity: 1
-				};
+			// Avatar images must be a minimum of 40 x 40 pixels
+			avatarStyles = {
+				height: 40,
+				width: 40
+			}
 
 		return (
 			<div className="Feedcard">
 				<Grid>
 					<Row>
-						<Col>
 
-							<div className="image" style={imageStyles}>
-								<div className="share-outer-circle" style={outerShareStyles}>
-									<div className="share-inner-circle" style={innerShareStyles}>
-										<img className="share" alt="share" src={share} style={innerShareStyles} />
-									</div>
-								</div>
+						<div className="image" style={imageStyles}>
+							<Image src={share} style={shareStyles} className="share" circle />
+						</div>
+
+					</Row>
+					<Row>
+
+						<div className="info">
+							<span className="chevron fa-stack" onClick={this.handleClick}>
+								<i className="fa fa-circle fa-stack-2x fa-inverse"></i>
+
+								{this.state.isTextExpanded ?
+									<i className="circles fa fa-chevron-down fa-stack-1x"></i> :
+									<i className="circles fa fa-chevron-up fa-stack-1x"></i>
+								}
+							</span>
+
+							<div className="title">Were dark matter filaments "predicted"?</div>
+						</div>
+
+					</Row>
+					<Row>
+
+						<div className="breadcrumbs">The History of the Birkeland Current > Clash of Worldviews > Noteworthy Online Discussions</div>
+
+					</Row>
+					<Row>
+
+						<div className="ratings">
+							<div style={chipStyles}>
+								<p>Testing  <Badge>9</Badge></p>
 							</div>
+						</div>
 
-							<div className="info">
+					</Row>
+					<Row>
 
-									<span className="chevron fa-stack" onClick={this.handleClick}>
-										<i className="fa fa-circle fa-stack-2x fa-inverse"></i>
-
-										{this.state.isTextExpanded ?
-											<i className="circles fa fa-chevron-down fa-stack-1x"></i> :
-											<i className="circles fa fa-chevron-up fa-stack-1x"></i>
-										}
-									</span>
-
-								<div className="title">Were dark matter filaments "predicted"?</div>
-
-								<div className="breadcrumbs">The History of the Birkeland Current > Clash of Worldviews > Noteworthy Online Discussions</div>
-
-								<div className="ratings">
-									<div style={chipStyles.wrapper}>
-										<Chip style={chipStyles.chip}>
-											<Avatar size={32}>9</Avatar>
-											Testing
-										</Chip>
-									</div>
-								</div>
-
-								<div className="author">
-									<div className="avatar">
-										<Avatar src={chris} />
-									</div>
-									<div className="author-info">
-										<div className="name">Chris Reeve</div>
-										<div className="role">Master of Controversies</div>
-									</div>
-								</div>
-
-								<div className="content">{h.parse('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus tellus dui, sit amet fermentum justo venenatis ut. Cras lacinia nisl bibendum, vehicula mauris sed, mollis nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit.')}</div>
+						<div className="author">
+							<div className="avatar">
+								<Image src={chris} style={avatarStyles} circle />
 							</div>
+							<div className="author-info">
+								<div className="name">Chris Reeve</div>
+								<div className="role">Master of Controversies</div>
+							</div>
+						</div>
 
-						</Col>
+					</Row>
+					<Row>
+
+						<div className="content">{h.parse('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus tellus dui, sit amet fermentum justo venenatis ut. Cras lacinia nisl bibendum, vehicula mauris sed, mollis nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit.')}</div>
+
 					</Row>
 				</Grid>
 			</div>
