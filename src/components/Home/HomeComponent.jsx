8a1690a16beb60f550bdb2af16f9f2f5c2f59913
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-07-20 11:06:36 -0700

    Able to clear out the search results when no query, but problem with removing ?query= from URL when empty

--- src/components/Home/HomeComponent.jsx
+++ src/components/Home/HomeComponent.jsx
@@ -6,24 +6,71 @@ import './Home.css';
 import elephant from '../../images/elephant.png';
 import { Grid } from 'react-bootstrap';
 
-// Algolia Search Dependencies
+// Algolia Search / React Router Integration Dependencies
 import { InstantSearch, Hits, SearchBox, Stats, Pagination } from 'react-instantsearch/dom';
 import SearchResult from '../SearchResult/SearchResult';
-
-// React Router Dependencies
+import qs from 'qs';
 import { withRouter } from 'react-router-dom';
 
 // Permits HTML markup encoding in feed text
 // import { Parser as HtmlToReactParser } from 'html-to-react';
 
+const
+	updateAfter = 700,
+	createURL = state => state ? `?${qs.stringify(state)}` : '',
+	searchStateToUrl = (props, searchState) =>
+		searchState ? `${props.location.pathname}${createURL(searchState)}` : '';
+
 class HomeComponent extends Component {
 	constructor(props) {
 		super(props);
 
+		this.state = {
+			searchState: qs.parse(props.location.search.slice(1))
+		};
+
 		this.props = props;
 	}
 
+	onSearchStateChange = searchState => {
+		clearTimeout(this.debouncedSetState);
+		this.debouncedSetState = setTimeout(() => {
+			this.props.history.push(
+				searchStateToUrl(this.props, searchState),
+				searchState
+			);
+		}, updateAfter);
+
+		this.setState({ searchState });
+	};
+
+	// componentWillReceiveProps(nextProps) {
+	// 	if (nextProps.router.location
+	// 		&& nextProps.router.location.state
+	// 		&& this.props.router.location
+	// 		&& this.props.router.location.state) {
+
+	// 		if (nextProps.router.location.state.query === '' &&
+	// 			this.props.router.location.state.query !== '') {
+
+	// 			this.props.history.push(
+	// 				searchStateToUrl(nextProps, ''),
+	// 				''
+	// 			);
+	// 			this.setState({ searchState: '' });
+
+	// 		}
+	// 	}
+	// }
+
 	render() {
+		// Be very careful with this: Do not assume that this.props.router.location.state is there!
+		const isSearch = this.props.router.location && this.props.router.location.search ? 
+			this.props.router.location.state.query !== '' :
+			false;
+
+		console.log('isSearch: ' + isSearch);
+
 		return (
 			<div className="Home">
 
@@ -35,7 +82,10 @@ class HomeComponent extends Component {
 				<InstantSearch
 					appId="HDX7ZDMWE9"
 					apiKey="f9898dbf6ec456d206e59bcbc604419d"
-					indexName="controversy_cards">
+					indexName="controversy_cards"
+					searchState={this.state.searchState}
+					onSearchStateChange={this.onSearchStateChange.bind(this)}
+					createURL={createURL}>
 
 					<Grid>
 
@@ -43,16 +93,14 @@ class HomeComponent extends Component {
 							className="SearchBox"
 							translations={{placeholder: 'Enter a Controversy'}} />
 
-						<Stats />
+						{ isSearch && <Stats /> }
+						{ isSearch && <Hits hitComponent={SearchResult} /> }
 
-						<Hits
-							hitComponent={SearchResult} />
-
-						<SearchResult />
+						{ isSearch && <SearchResult /> }
 
 					</Grid>
 
-					<Pagination showLast />
+					{ isSearch && <Pagination showLast /> }
 
 				</InstantSearch>
 
