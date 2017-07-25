bf147fefaff3897e88bbe52f5da0a04ffed7fe16
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-07-24 20:33:49 -0700

    Removed unused code in CardStack

--- src/swipes/CardStack/CardStackComponent.jsx
+++ src/swipes/CardStack/CardStackComponent.jsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 
 // UI Dependencies
 import { Grid, Row } from 'react-bootstrap';
-// import debounce from 'debounce';
 import './CardStack.css';
 
 // Components
@@ -14,8 +13,6 @@ import Comments from '../../components/Comments/Comments.jsx';
 import Card from '../../components/Card/Card.jsx';
 import FeedCard from '../../components/FeedCard/FeedCard.jsx';
 import FeedCardList from '../../components/FeedCardList/FeedCardList.jsx';
-// import SwipeOverlay from '../../components/SwipeOverlay/SwipeOverlay.jsx';
-// import injectTapEventPlugin from 'react-tap-event-plugin';
 
 // React Router Dependencies
 import { withRouter } from 'react-router-dom';
@@ -24,15 +21,8 @@ class CardStackComponent extends Component {
 	constructor(props) {
 		super(props);
 
-		// injectTapEventPlugin();
-
-		this.state = {
-			id: null
-		}
-
 		this.props = props;
 		this.handleSwipe = this.handleSwipe.bind(this);
-		// this.changeRoute = this.changeRoute.bind(this);
 
 		this.levels = [
 			'worldview',
@@ -41,17 +31,9 @@ class CardStackComponent extends Component {
 			'conceptual',
 			'narrative'
 		];
-
-		// // Testing
-		// this.route = '/' + this.props.match.params.controversy +
-		// 	'/' + this.levels[this.props.discourse.level] +
-		// 	(this.props.discourse.level === 0 ? '/card' : '');
 	}
 
 	componentDidMount() {
-		// this.deactivateSwipeOverlay = debounce(this.props.deactivateSwipeOverlay, this.props.discourse.isFullScreen ? 3000 : 6000);
-
-		// window.onscroll = function () { window.scrollTo(0, 0); };
 	}
 
 	handleSwipe(index, previous) {
@@ -59,52 +41,22 @@ class CardStackComponent extends Component {
 			swipeDirection = index > previous ? 'right' : 'left';
 
 		this.props.setCardStackLevel(index, swipeDirection);
-		// this.handleSwipeOverlay();
-	}
-
-	// When we change routes, we need to update the URL
-	changeRoute() {
-		// this.props.history.push(route);
-	}
-
-	handleSwipeOverlay() {
-		// this.props.activateSwipeOverlay();
-		// this.deactivateSwipeOverlay();
 	}
 
 	showSettings(event) {
 		event.preventDefault();
 	}
 
-	componentWillReceiveProps(nextProps) {
-		// if (nextProps.discourse.level !== this.props.discourse.level) {
-		// 	this.deactivateSwipeOverlay();
-		// }
-	}
-
 	render() {
 		const
-			// h = new HtmlToReactParser(),
-
 			containerStyles = {
 				height: '100vh'
 			};
-
-			// currentLevel = this.levels[this.props.discourse.level];
 
 		return (
 			<div ref={c => this.container = c} className="CardStack">
 				<Grid>
 					<Row>
-					
-{/*
-						<SwipeOverlay
-							isFullScreen={this.props.discourse.isFullScreen}
-							discourseLevel={this.props.discourse.level}
-							active={2}
-							discourseHandler={this.props.setDiscourseLevel}
-							deactivateOverlayHandler={this.props.deactivateSwipeOverlay} />
-*/}
 
 						<SwipeableViews
 							axis='x'
@@ -112,8 +64,7 @@ class CardStackComponent extends Component {
 							resistance
 							ignoreNativeScroll
 							index={this.props.cardStack.level}
-							onChangeIndex={this.handleSwipe}
-							onTransitionEnd={this.changeRoute}>
+							onChangeIndex={this.handleSwipe}>
 
 							<div className="Browser">
 								<Browser />
